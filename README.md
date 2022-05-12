2843d39e2ed05309cd74c272c293ee53e52d1b04
leetcore <leetcore@live.de>
2022-05-12 22:23:35 +0200

    init script

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# vulnscanner-simple
-Automatisierung von Amass + Nmap.
+# Vulnscanner Automatisierung
+Automatisierung von Amass + Nmap + Nikto.
+
+# Setup
+Du solltest Amass, Nmap und Nikto installiert haben. Am besten direkt in
+Kali Linux starten!
+
+# Nutzung
+``` bash
+python3 scanner.py
+```
+Du wirst interaktiv nach dem Domainnamen gefragt.
+Gibt den Domainnamen ohne http// oder www. ein
+Zum Beispiel nur: scanme.nmap.org
+
+# Auswertung
+Suche in nmap.txt nach "vulners:" Das zeigt die gefundenen Sicherheitslücken an.
+Die Informationen basieren auf den erkannten Versionsnummern und können deshalb 
+auch falsch sein, wenn die falsche Versionsnummer ausgegeben wird.
---
+++ scanner.py
@@ -0,0 +1,53 @@
+from shlex import quote
+import os
+import argparse
+import re
+
+def main():
+    # ask for target domain
+    print("Target domain:")
+    domain = input()
+    start_scanner(domain)
+
+def start_scanner(domain: str):
+    domain = quote(domain.strip().lower())
+    target_path = f"scans/{domain}/"
+
+    # domain validation
+    is_domain = re.fullmatch(
+        "^(?!-)[A-Za-z0-9-]+([\\-\\.]{1}[a-z0-9]+)*\\.[A-Za-z]{2,20}$", domain
+    )
+    if not is_domain:
+        print("Invalid domain")
+        return
+
+    # Add a pretty banner
+    print(
+        """
+/..---..---..---..---..----..---. .---.
+||  -||  -||'-/ /| |  | || || |-< | |- 
+--'---''---' '-' '---''----''-''-''---'
+Amass + Nmap + Nikto Automatisierung von 1337core
+	"""
+    )
+
+    # check if folder exists already
+    if os.path.exists(target_path) == True:
+        print(f"[-] Path in {target_path} already exist!")
+    else:
+        os.system(f"mkdir -p {target_path}")
+    
+    # run amass
+    os.system(f"amass enum -max-dns-queries 20 -w dns.txt -d {domain} -o {target_path}amass.txt")
+
+    # run nmap
+    os.system(f"nmap -A -v -Pn --top-ports 1337 --script vulners -T4 --host-timeout 5m --open -iL {target_path}amass.txt -oN {target_path}nmap.txt -oX {target_path}nmap.xml")
+
+    # run nikto
+    os.system(f"nikto -host {target_path}amass.txt -Tuning 23457890abcde -port 80,443,1337,8080,8081 -maxtime 5m -o {target_path}nikto.txt")
+
+    # TODO: port -> show service, show script vulners -> cve
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple Amass + Nmap Scanner")
+    main()
