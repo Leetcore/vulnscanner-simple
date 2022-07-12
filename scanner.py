ffd4c9a5a35856cbb35bfe45f555217f64f38a78
leetcore <leetcore@live.de>
2022-07-12 18:35:24 +0200

    switch to vulscan and amass passive

--- scanner.py
+++ scanner.py
@@ -11,7 +11,7 @@ def main():
 
 def start_scanner(domain: str):
     domain = quote(domain.strip().lower())
-    target_path = f"scans/{domain}/"
+    target_path = f"scans/{domain}"
 
     # domain validation
     is_domain = re.fullmatch(
@@ -35,29 +35,31 @@ Amass + Nmap + Nikto Automatisierung von 1337core
     if os.path.exists(target_path) == True:
         print(f"[-] Path in {target_path} already exist!")
     else:
-        os.system(f"mkdir -p {target_path}")
+        os.system(f"mkdir -p {target_path}/")
     
     # run amass
-    os.system(f"amass enum -max-dns-queries 20 -w dns.txt -nolocaldb -d {domain} -o {target_path}amass.txt")
+    os.system(f"amass enum -passive -nolocaldb -d {domain} -o {target_path}/amass.txt")
+    os.system(f"echo {domain} >> {target_path}/amass.txt")
 
     # run nmap
-    os.system(f"nmap -A -v --top-ports 1337 --script vulners --host-timeout 15m --open -iL {target_path}amass.txt -oN {target_path}nmap.txt -oX {target_path}nmap.xml -oG {target_path}nmap_grep.txt")
+    os.system(f"nmap -sV -v --top-ports 50 -T5 --script=vulscan/vulscan.nse --host-timeout 5m --script-args vulscandb=cve.csv --open -iL {target_path}/amass.txt -oN {target_path}/nmap.txt -oX {target_path}/nmap.xml -oG {target_path}/nmap_grep.txt")
 
     # get webserver from nmap scan
-    with open(f"{target_path}nmap_grep.txt", "r") as myfile:
+    with open(f"{target_path}/nmap_grep.txt", "r") as myfile:
         content_array = myfile.readlines()
         webservers: list[str] = []
         for line in content_array:
-            if "/open/tcp//http" in line:
+            if "/open/tcp//http" in line or "/open/tcp//ssl|https" in line:
                 host_match = re.search('Host: ([0-9\\.]+)', line)
                 if isinstance(host_match, re.Match):
-                    ip = host_match.group(1)
+                    host = host_match.group(1)
                     ports = re.findall('(\\d+)/open/tcp//http', line)
+                    ports += re.findall('(\\d+)/open/tcp//ssl\\|https', line)
                     for port in ports:
-                        webservers.append(f"{ip}:{port}")
+                        webservers.append(f"{host}:{port}")
 
 
-    with open(f"{target_path}webserver.txt", "w") as myfile:
+    with open(f"{target_path}/webserver.txt", "w") as myfile:
         myfile.write("\n".join(webservers))
 
     for webserver in webservers:
@@ -65,8 +67,8 @@ Amass + Nmap + Nikto Automatisierung von 1337core
         port = webserver.split(":")[1]
 
         # run nikto
-        os.system(f"nikto -host {ip} -port {port} -Tuning 23457890abcde -timeout 3 -maxtime 5m -useragent 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36' -o {target_path}nikto.txt")
+        os.system(f"nikto -host {ip} -port {port} -Tuning 23457890abcde -timeout 3 -maxtime 5m -useragent 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36' -o {target_path}/nikto.txt")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Simple Amass + Nmap Scanner")
+    parser = argparse.ArgumentParser(description="Simple Amass, Nmap + Nikto Scanner")
     main()
--- setup.sh
+++ setup.sh
@@ -1,1 +1,24 @@
-apt install python3 amass nmap nikto
+if [[ "$OSTYPE" == "linux"* ]]; then
+    echo "install amass nmap nikto"
+    apt install git python3 amass nmap nikto
+fi
+
+echo "nmap vulscan script"
+if [[ -d "/usr/share/nmap/scripts/" ]]
+then
+    cd /usr/share/nmap/scripts/
+    git clone https://github.com/scipag/vulscan.git
+    cd vulscan
+    git fetch
+    curl https://www.computec.ch/projekte/vulscan/download/cve.csv -o cve.csv
+fi
+
+# homebrew version
+if [[ -d "/opt/homebrew/Cellar/nmap/7.92/share/nmap/scripts/" ]]
+then
+    cd /opt/homebrew/Cellar/nmap/7.92/share/nmap/scripts/
+    git clone https://github.com/scipag/vulscan.git
+    cd vulscan
+    git fetch
+    curl https://www.computec.ch/projekte/vulscan/download/cve.csv -o cve.csv
+fi
